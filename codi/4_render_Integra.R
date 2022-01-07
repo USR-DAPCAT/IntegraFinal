f11e03140c0b8d53300d8f7a422b7323fb404e2e
Ramon Puig Treserres <raypuig@gmail.com>
2022-01-07 17:50:17 +0100

    actualitazcio

---
+++ codi/4_render_Integra.R
@@ -0,0 +1,55 @@
+############
+#07.01.2022#
+#
+#
+# -------------------------   PARAMETRES  --------------------------------------------------------
+gc()
+rm(list=ls())
+dir_dades_origen="../../DADES/INTEGRA" 
+dir_dades_desti="dades"
+nom_output="Poblacio"
+save.image("codi/params.Rdata")
+# -----------------------------------------------------------
+
+
+
+#---------------------------- LECTURA1 ------------------------------------------------------------# 
+rm(list=ls())
+gc()
+load("codi/params.Rdata")
+
+#--------------------------------------------------------------------------------------------------# 
+rmarkdown::render(input="./codi/1_lectura_Integra.RMD",
+                  output_file=paste0(here::here("resultats/Informe_Lectura_INTEGRA_"),nom_output,"_",Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
+#--------------------------------------------------------------------------------------------------#
+#---------------------------  PREPARACIO ----------------------------------------------------------#
+library("dplyr")
+rm(list=ls())
+gc()
+load("codi/params.Rdata")
+#--------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------#
+#rmarkdown::render(input="./codi/2_preparacio_HTG.RMD",
+#                  output_file=paste0(here::here("resultats/Informe_preparcio_HTG_"),nom_output,Sys.Date()),
+#                  params = list(dir_dades_desti=dir_dades_desti))
+#
+#--------------------------------------------------------------------------------------------------#
+#---------------------------  ANALISI -------------------------------------------------------------#
+library("dplyr")
+rm(list=ls())
+gc()
+load("codi/params.Rdata")
+#--------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------#
+rmarkdown::render(input="./codi/3_analisi_Integra.RMD",
+                  output_file=paste0(here::here("resultats/Informe_Exploratori_INTEGRA_"),"_",nom_output,"_",Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti))
+#--------------------------------------------------------------------------------------------------#
+
+
+
+
+
+
+
